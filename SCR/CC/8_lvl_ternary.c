64c2796a7521c7309067b5143d1763162c298be9
AAYUSH <acchourasiya2007@gmail.com>
2025-11-19 20:45:59 +0530

    Delete SCR/CC directory

--- SCR/CC/8_lvl_ternary.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    // expression like if else ?(agar) :(nhi toh)
-     int a = 24;int b = 44;
-     a>b?printf("a is grater"):printf("b is greater");
-    return 0;
-}
